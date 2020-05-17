96d7a1473e42ce68f3e27ee32ee051ebbcbcecbc
Abhinav Tripathi <abhinavtripathi95@gmail.com>
2020-05-18 02:47:58 +0530

    Add files via upload

---
+++ notify-me.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)";
+
+battery_status=`acpi -b | grep -E -o 'Discharging||Charging'`
+battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
+
+#IMP: always write absolute paths for cronjob
+if [ $battery_status = 'Discharging' ]
+then
+
+    if [ $battery_level -le 15 ] 
+    then
+        /usr/bin/notify-send "Battery critical" "Shutting down in 30 seconds" -i /home/abhinav/Documents/battery-scripts/alert.ico
+        sleep 30
+        # sudo chmod u+s /sbin/shutdown
+        /sbin/shutdown -h now
+
+    elif [ $battery_level -le 20 ]
+    then
+        /usr/bin/notify-send "Battery very low" "Battery level is ${battery_level}%!" -i /home/abhinav/Documents/battery-scripts/battery-critical.ico
+
+    elif [ $battery_level -le 30 ]
+    then 
+        /usr/bin/notify-send "Battery low" "Battery level is ${battery_level}%!" -i /home/abhinav/Documents/battery-scripts/battery-low.ico
+    
+    fi
+
+else
+    if [ $battery_level -gt 92 ]
+    then
+        /usr/bin/notify-send "Battery is charging" "Battery level is ${battery_level}%!" -i /home/abhinav/Documents/battery-scripts/battery-full.ico
+    fi
+fi
